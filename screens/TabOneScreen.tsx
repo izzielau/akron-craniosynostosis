414ce204929e9ece0483aca3121feed783e54715
rebeccama <r69ma@uwaterloo.ca>
2021-02-15 19:56:57 -0500

    proof of concept for camera view

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,17 +1,46 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import CameraView from './CameraView';
 
 export default function TabOneScreen() {
-  return (
+  const [cameraOn, setCameraOn] = useState(false)
+
+  /* possibly use react-navigation in the future */
+  if(cameraOn){
+    return (<CameraView />);
+  }
+
+  return(
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+      <TouchableOpacity
+          onPress={()=>{
+            setCameraOn(true)
+          }}
+          style={{
+            width: 130,
+            borderRadius: 4,
+            backgroundColor: '#14274e',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 40
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Take picture
+          </Text>
+        </TouchableOpacity>
+    </View>);
 }
 
 const styles = StyleSheet.create({
