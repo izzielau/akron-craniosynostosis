62f263f38c00caac5d80ce7c6f28ed944e31e618
Izzie Lau <isabellalau@berkeley.edu>
2021-04-12 14:27:36 -0700

    fixed issues with multiple imports in checklistscreen.jsx

--- screens/ChecklistScreen.jsx
+++ screens/ChecklistScreen.jsx
@@ -8,63 +8,56 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import Constants from 'expo-constants';
-import Collapsible from 'react-native-collapsible';
-import Accordion from 'react-native-collapsible/Accordion';
-import { useFonts } from 'expo-font';
-
-import { styles } from "../constants/Styles";
-import { useEffect } from "react";
-import { shuffle } from "../utils/ArrayUtils";
+import Accordion from 'react-native-collapsible';
 
  export default function ChecklistScreen(props) {
   const { navigation } = props;
-const CONTENT = [
-  {
-    title: '20-30 minutes',
-    content: 'This whole process should take up to 30 minutes total. Keep in mind that an early time investment now will help later on.',
-  },
-  {
-    title: 'Yourself and an assistant',
-    content: 'It takes two people to successfully take all the pictures for this application, so make sure you have someone to help you. ',
-  },
-  {
-    title: 'A well-lit room',
-    content: 'Natural lighting significantly improves photo quality',
-  },
-  {
-    title: 'Solid background',
-    content: 'A solid background is helpful to provide that makes the image clear enough for proper evaluation. Finding a blank wall or hanging up a bedsheet might work in some cases.',
-  },
-  {
-    title: 'Towel or blanket',
-    content: 'Wrapping the baby in a blanket can help with some top-down views ',
-  },
+  const CONTENT = [
+    {
+      title: '20-30 minutes',
+      content: 'This whole process should take up to 30 minutes total. Keep in mind that an early time investment now will help later on.',
+    },
+    {
+      title: 'Yourself and an assistant',
+      content: 'It takes two people to successfully take all the pictures for this application, so make sure you have someone to help you. ',
+    },
+    {
+      title: 'A well-lit room',
+      content: 'Natural lighting significantly improves photo quality',
+    },
+    {
+      title: 'Solid background',
+      content: 'A solid background is helpful to provide that makes the image clear enough for proper evaluation. Finding a blank wall or hanging up a bedsheet might work in some cases.',
+    },
+    {
+      title: 'Towel or blanket',
+      content: 'Wrapping the baby in a blanket can help with some top-down views ',
+    },
 
-  {
-    title: 'Water',
-    content: 'If baby has a lot of hair, wetting it will make their skull shape easier to see.',
-  },
-  {
-    title: 'Patience',
-    content: "Lastly, patience. We recognize that infants aren't known for cooperating, but encourage you to make the best effort now so we can provide the most accurate evaluation.",
-  },
-];
+    {
+      title: 'Water',
+      content: 'If baby has a lot of hair, wetting it will make their skull shape easier to see.',
+    },
+    {
+      title: 'Patience',
+      content: "Lastly, patience. We recognize that infants aren't known for cooperating, but encourage you to make the best effort now so we can provide the most accurate evaluation.",
+    },
+  ];
 
-const SELECTORS = [
-  {
-    title: 'First',
-    value: 0,
-  },
-  {
-    title: 'Third',
-    value: 2,
-  },
-  {
-    title: 'None',
-  },
-];
+  const SELECTORS = [
+    {
+      title: 'First',
+      value: 0,
+    },
+    {
+      title: 'Third',
+      value: 2,
+    },
+    {
+      title: 'None',
+    },
+  ];
 
- class App extends Component {
   state = {
     activeSections: [],
     collapsed: true,
@@ -93,7 +86,7 @@ const SELECTORS = [
     );
   };
 
-  renderContent(section, _, isActive) {
+  renderContent = (section, _, isActive) => {
     return (
       <View
         duration={400}
@@ -107,34 +100,33 @@ const SELECTORS = [
     );
   }
 
-  render() {
-    const { multipleSelect, activeSections } = this.state;
-    
-    return (
-      <View style={styles.container}>
-        <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
-          <View><Text style = {{color: '#9A9AB0', fontSize: 18, padding: 15, fontWeight: '700'}}>What will you need?</Text></View>
-          <Accordion
-            activeSections={activeSections}
-            sections={CONTENT}
-            touchableComponent={TouchableOpacity}
-            expandMultiple={multipleSelect}
-            renderHeader={this.renderHeader}
-            renderContent={this.renderContent}
-            duration={400}
-            onChange={this.setSections}
-          />
-        </ScrollView>
-        <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Introduction")}
-        >
-        <Text style={styles.buttonText}>Next</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-  };
+  const { multipleSelect, activeSections } = this.state;
+  
+  return (
+    <View style={styles.container}>
+      {/* 
+      <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
+        <View><Text style = {{color: '#9A9AB0', fontSize: 18, padding: 15, fontWeight: '700'}}>What will you need?</Text></View>
+        <Accordion
+          activeSections={activeSections}
+          sections={CONTENT}
+          touchableComponent={TouchableOpacity}
+          expandMultiple={multipleSelect}
+          renderHeader={this.renderHeader}
+          renderContent={this.renderContent}
+          duration={400}
+          onChange={this.setSections}
+        />
+      </ScrollView>
+      */}
+      <TouchableOpacity
+      style={styles.button}
+      onPress={() => navigation.navigate("Introduction")}
+      >
+      <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 
@@ -162,13 +154,12 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     padding: 10,
     color: 'rgba(104,107,126,1)',
-
   },
-content: {
-color: 'rgba(154,154,176,1)',
-fontWeight: '700',
-fontSize: 16,
-padding:20,
+  content: {
+    color: 'rgba(154,154,176,1)',
+    fontWeight: '700',
+    fontSize: 16,
+    padding:20,
   },
   active: {
     backgroundColor: 'rgba(255,255,255,1)',
@@ -204,4 +195,3 @@ padding:20,
     marginRight: 8,
   },
 });
-export{App};
