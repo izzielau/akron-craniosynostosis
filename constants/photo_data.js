424a4ea2667f24655b8ea8f7e9966ca0e648aa4f
wyang0216 <william_yang@brown.edu>
2021-05-07 21:59:14 -0400

    add rest of pictures into slider

--- constants/photo_data.js
+++ constants/photo_data.js
@@ -1,23 +1,28 @@
 export default photo_data = [
     {
-      title: "Aenean leo",
-      body: "Ut tincidunt tincidunt erat. Sed cursus turpis vitae tortor. Quisque malesuada placerat nisl. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+      title: "Angled Top",
+      body: "This is the angled from the top.",
       imgUrl: require('app/assets/baby_photos/angled-top.png')
-      // '/Users/williamyang/Documents/GitHub/akron/assets/tutorial/filler.jpg'
-      // imgUrl: "https://picsum.photos/id/11/200/300"
     },
     {
       title: "In turpis",
       imgUrl: require('app/assets/baby_photos/back.png')
     },
     {
-      title: "Lorem Ipsum",
-      body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
       imgUrl: require('app/assets/baby_photos/front.png')
     },
     {
-      title: "Lorem Ipsum",
-      body: "Phasellus ullamcorper ipsum rutrum nunc. Nullam quis ante. Etiam ultricies nisi vel augue. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc.",
+      title: "Worm Eye",
+      body: "This is the worm eye angle.",
       imgUrl: require('app/assets/baby_photos/worm-eye.png')
+    },
+    {
+      imgUrl: require('app/assets/baby_photos/top.png')
+    },
+    {
+      imgUrl: require('app/assets/baby_photos/left-side.png')
+    },
+    {
+      imgUrl: require('app/assets/baby_photos/right-side.png')
     }
   ]
