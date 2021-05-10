00eda025301271d173a1456229990fe2bf4d33c8
Brandon Lai <brandon.lai@vanderbilt.edu>
2021-05-09 20:56:21 -0500

    implements folder linking to camera roll with carousel and hardcoded dimensions (will update with relative screen dimensions

--- screens/FolderView.jsx
+++ screens/FolderView.jsx
@@ -1,13 +1,17 @@
-import React, { Component, version, useEffect, useState } from "react";
-import { Image, Text, View, TouchableOpacity, FlatList, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, Text, View, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
 import { styles } from "../constants/Styles";
 import * as MediaLibrary from 'expo-media-library';
+import Carousel, { Pagination } from "react-native-snap-carousel";
 
 export default function FolderView(props) {
     const { navigation } = props;
     const { route } = props;
 
     const { title } = route.params;
+
+    const imageWidth = 325;
+    const imageHeight = 250;
 
     useEffect(() => {
         navigation.setOptions({headerTitle: title});
@@ -19,7 +23,6 @@ export default function FolderView(props) {
         (async () => {
             const rollPermissions = await MediaLibrary.requestPermissionsAsync();
             setHasCameraRollPermission(rollPermissions.status === 'granted');
-            // console.log("setting camera roll permission to ", rollPermissions.status === 'granted')
         })();
     }, []);
 
@@ -28,45 +31,59 @@ export default function FolderView(props) {
     useEffect(() => {
         (async () => {
             const album = await MediaLibrary.getAlbumAsync(title);
-            // console.log(album);
             const res = await MediaLibrary.getAssetsAsync({ album });
             setAlbumAssets(res.assets);
-            // console.log(albumAssets)
         })();
     }, []);
 
     const renderItem = ({ item }) => (
         <Image 
-            style={[styles.galleryFolder, {height: 300, width: 250}]} 
+            style={[styles.galleryFolder, folderStyles.image]} 
             source={item} />
-      )
+    )
     
-
     return (
         <View style={[styles.container, {justifyContent: 'flex-start'}]}>
-            <Text style={[styles.bodyText, {marginTop: 10, marginBottom: 10}]}>
+            <Text style={[styles.bodyText, folderStyles.text]}>
                 {"Compare to example."}
             </Text>
-            <View style={[styles.galleryFolder, {height: 600, width: 325}]}>
-                <FlatList 
-                    data={albumAssets} 
-                    renderItem={renderItem}
-                    keyExtractor={item => item.id}
-                    numColumns={1}
-                    scrollEnabled={true}
-                />
-                <Text style={styles.bodyText}>Your Image</Text>
-            </View>
-            
-            {/* <View style={[styles.galleryFolder, {height: 200, width: 300}]}>
-                <Text style={styles.bodyText}>Example Image</Text>
-            </View>
+            <Text style={[styles.bodyText, folderStyles.sectionText]}>Your Image</Text> 
+            <Carousel
+                data={albumAssets}
+                renderItem={renderItem}
+                sliderWidth={imageWidth}
+                sliderHeight={imageHeight}
+                itemWidth={imageWidth}
+                layout={'stack'}
+                layoutCardOffset={294}
+            />
+            <Text style={[styles.bodyText, folderStyles.sectionText]}>Example Image</Text>
+            <View style={[styles.galleryFolder, folderStyles.image]}></View>
             <TouchableOpacity
-                style={[styles.button, {marginTop: 10}]}
+                style={[styles.button, folderStyles.sectionText, {marginBottom: 25}]}
                 onPress={() => navigation.navigate("TBD")}
             >
                 <Text style={styles.buttonText}>Capture More</Text>
-            </TouchableOpacity> */}
+            </TouchableOpacity>
         </View>
     )
 }
+
+const folderStyles = StyleSheet.create({
+    text: {
+        marginTop: 10, 
+        marginBottom: 0
+    },
+    sectionText: {
+        marginTop: 10, 
+        marginBottom: 10
+    },
+    image: {
+        height: 250, 
+        width: 325, 
+        margin: 0, 
+        padding: 0, 
+        alignItems: 'center', 
+        justifyContent: 'center'
+    }
+})
