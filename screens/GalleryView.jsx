cf6a4c733ad2aad5e0817800b509f7891b6c595b
Brandon Lai <brandon.lai@vanderbilt.edu>
2021-05-29 11:58:42 -0500

    updates galleryview

--- screens/GalleryView.jsx
+++ screens/GalleryView.jsx
@@ -1,10 +1,9 @@
 import React, { useState, usela } from "react";
 import { Image, Text, View, TouchableOpacity, FlatList, Dimensions } from "react-native";
-
 import { styles } from "../constants/Styles";
 import { useEffect } from "react";
-import { shuffle } from "../utils/ArrayUtils";
 import * as MediaLibrary from 'expo-media-library';
+import Images from '../constants/Constants';
 
 export default function GalleryView(props) {
   const { navigation } = props;
@@ -12,36 +11,38 @@ export default function GalleryView(props) {
   const numColumns = 2;
   
   const [angles] = useState([
-    {title: 'Front', key: '1', image: null},
-    {title: 'Back', key: '2', image: null},
-    {title: 'Left Side', key: '3', image: null},
-    {title: 'Right Side', key: '4', image: null},
-    {title: "Worm's Eye", key: '5', image: null},
-    {title: 'Top', key: '6', image: null},
-    {title: 'Top Angled', key: '7', image: null},
+    {title: 'Front', key: '1', image: null, example: require("../assets/updated-faces/front.png")},
+    {title: 'Back', key: '2', image: null, example: Images.back},
+    {title: 'Left Side', key: '3', image: null, example: Images.leftSide},
+    {title: 'Right Side', key: '4', image: null, example: Images.rightSide},
+    {title: "Worm's Eye", key: '5', image: null, example: Images.wormsEye},
+    {title: 'Top', key: '6', image: null, example: Images.top},
+    {title: 'Top Angled', key: '7', image: null, example: Images.topAngled},
     // {title: 'Angle 8', key: '8', image: ''},
   ]);
 
   useEffect(() => {
-    angles.forEach((item) => {
-      (async () => {
-        const album = await MediaLibrary.getAlbumAsync(item.title);
-        const res = await MediaLibrary.getAssetsAsync({ album });
-        item.image = res.assets;
-      })();
-    })});
+      angles.forEach((item) => {
+        (async () => {
+          const album = await MediaLibrary.getAlbumAsync(item.title);
+          const res = await MediaLibrary.getAssetsAsync({ album });
+          item.image = res.assets;
+        })();
+      })
+  });
 
   const renderItem = ({ item }) => (
     <TouchableOpacity 
       onPress={() => navigation.navigate("Folder", {
-        title: item.title
+        title: item.title,
+        key: item.key
       })}
     >
       <Image
         style={styles.galleryFolder} 
         source={ item.image } 
       />
-      <View style={[styles.button, {position: 'absolute', top: Dimensions.get('window').height / 40, left: 35, backgroundColor: 'rgba(240, 240, 247, 0.75)', width: Dimensions.get('window').width / 3.5, height: Dimensions.get('window').height / 25}]}>
+      <View style={[styles.button, {position: 'absolute', top: Dimensions.get('window').height / 40, left: Dimensions.get('window').width / 10.5, backgroundColor: 'rgba(240, 240, 247, 0.75)', width: Dimensions.get('window').width / 3.5, height: Dimensions.get('window').height / 25}]}>
         <Text style={[styles.buttonText, {color: 'black'}]}>{item.title}</Text>
       </View>
     </TouchableOpacity>
@@ -66,7 +67,7 @@ export default function GalleryView(props) {
         renderItem={renderItem}
         keyExtractor={item => item.key}
         numColumns={numColumns}
-        scrollEnabled={false}
+        scrollEnabled={true}
         style={{marginTop: Dimensions.get('window').height / 100}}
       />
 
